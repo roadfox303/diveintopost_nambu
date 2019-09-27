b52bb77886af4deeb013a3e6962702b584e69df2
takamori nanbu <roadfox303@gmail.com>
2019-09-27 20:51:12 +0900

    アジェンダ削除機能

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -6,4 +6,14 @@ class NoticeMailer < ApplicationMailer
     @team = team
     mail to: User.find(@team.owner_id).email , subject: 'リーダー権限を移譲されました'
   end
+
+  def delete_agenda(agenda,destroyer)
+    @agenda = agenda
+    @destroyer = destroyer
+    sending = []
+    Team.find(@agenda.team_id).assign_users.each do |user|
+      sending << user.email
+    end
+    mail to: sending , subject: "アジェンダ#{@agenda.title}は削除されました"
+  end
 end
